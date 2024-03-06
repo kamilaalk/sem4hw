70b48d20f4e55500884c759117b0533d049daab8
Kamila <kamila.sbz@yandex.com>
2024-03-06 18:59:39 +0500

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿while (true){
+Console.Write("Введите число или 'q' для выхода: ");
+string input = Console.ReadLine();
+if (input == "q"){
+    break;
+}
+
+int num = Convert.ToInt32(input);
+int sum = 0;
+while (num > 0){
+    sum += num % 10;
+    num /= 10;
+}
+if (sum % 2 == 0)
+{
+Console.WriteLine("[STOP]");
+break;
+}
+}
