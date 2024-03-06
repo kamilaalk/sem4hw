fa032d350e183329944f23d1279bc5b2c9f19644
Kamila <kamila.sbz@yandex.com>
2024-03-06 22:11:21 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int [] numbers = {1, 4, 6, 9, 3, 4};
+int temp;
+
+Console.Write("Исходный массив: ");
+foreach (int number in numbers)
+{
+Console.Write(number + " ");
+}
+
+for (int i = 0; i < numbers.Length / 2; i++)
+{
+temp = numbers[i];
+numbers[i] = numbers[numbers.Length - 1 - i];
+numbers[numbers.Length - 1 - i] = temp;
+}
+
+Console.Write("\nПеревернутый массив: ");
+foreach (int number in numbers)
+{
+    Console.Write(number + " ");
+}
