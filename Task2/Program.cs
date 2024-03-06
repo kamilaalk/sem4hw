c01b63a3ce910eb9c61379932de908f9aa8f16a4
Kamila <kamila.sbz@yandex.com>
2024-03-06 22:03:36 +0500

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] numbers = new int [10];
+Random random = new Random();
+int CountEven = 0;
+
+for(int i = 0; i < numbers.Length; i++){
+    numbers[i] = random.Next(100, 1000);
+    Console.Write(numbers[i] + " "); 
+if (numbers[i] % 2 == 0)
+{
+CountEven++;
+}
+}
+Console.WriteLine($"Количество четных чисел в массиве: {CountEven}");
